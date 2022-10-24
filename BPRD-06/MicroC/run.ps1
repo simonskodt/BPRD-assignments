6fff562061edff2cc1a3c0be81ce283992d66b36
simonskodt <simon@skodt.dk>
2022-10-24 11:19:30 +0200

    fix: change ps script

--- BPRD-06/MicroC/run.ps1
+++ BPRD-06/MicroC/run.ps1
@@ -1,3 +1,3 @@
 fslex --unicode CLex.fsl
 fsyacc --module CPar CPar.fsy
-dotnet fsi -r .\FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Interp.fs ParseAndRun.fs
+dotnet fsi -r ../../libraries/FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Interp.fs ParseAndRun.fs
