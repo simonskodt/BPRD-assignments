9647db81727fb28c79e6aa7f93cb556a47e25e48
simonskodt <simon@skodt.dk>
2022-10-24 11:18:49 +0200

    fix: change sh script

--- BPRD-06/run.sh
+++
@@ -1,3 +0,0 @@
-   fslex --unicode ./MicroC/CLex.fsl
-   fsyacc --module CPar ./MicroC/CPar.fsy
-   fsharpi -r ./MicroC/FsLexYacc.Runtime.dll ./MicroC/Absyn.fs ./MicroC/CPar.fs ./MicroC/CLex.fs ./MicroC/Parse.fs ./MicroC/Interp.fs ./MicroC/ParseAndRun.fs
