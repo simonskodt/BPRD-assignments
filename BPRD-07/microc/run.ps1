d4869b232c13d985cc1942cc91e2c33dd70c8637
simonskodt <simon@skodt.dk>
2022-11-02 21:38:25 +0100

    chore: setup to solve 8.4

--- BPRD-07/microc/run.ps1
+++ BPRD-07/microc/run.ps1
@@ -1,3 +1,3 @@
 fslex --unicode CLex.fsl
 fsyacc --module CPar CPar.fsy
-dotnet fsi -r ..\..\libraries\FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Machine.fs Comp.fs ParseAndComp.fs
+dotnet fsi -r ..\..\libraries\FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Interp.fs ParseAndRun.fs Machine.fs Comp.fs ParseAndComp.fs
