293a44e0800ec59f848f05621e91a8d18b1361cb
simonskodt <simon@skodt.dk>
2022-11-21 11:35:41 +0100

    feat(exercise): complete exercise 11

--- BPRD-10/Cont/Icon.fs
+++ BPRD-10/Cont/Icon.fs
@@ -31,6 +31,7 @@ type expr =
   | Write of expr
   | If of expr * expr * expr
   | Prim of string * expr * expr 
+  | Prim1 of string * expr       // exercise 11.8(iii)
   | And of expr * expr
   | Or  of expr * expr
   | Seq of expr * expr
@@ -88,6 +89,22 @@ let rec eval (e : expr) (cont : cont) (econt : econt) =
               | _ -> Str "unknown prim2")
               econt1)
           econt
+    | Prim1(ope, e) -> 
+      eval e (fun v -> fun econt1 ->
+        match (ope, v) with
+        | ("sqr", Int i) ->
+          cont (Int(i*i)) econt
+        | ("even", Int i) ->
+          if i % 2 = 0 then
+            cont (Int i) econt
+          else
+            econt1 ()
+        | ("multiples", Int i) ->
+          let rec aux x =
+            cont (Int(i+x)) (fun () -> aux (i+x)) 
+          aux i
+        | _ -> Str "unkown prim1")
+        econt
     | And(e1, e2) -> 
       eval e1 (fun _ -> fun econt1 -> eval e2 cont econt1) econt
     | Or(e1, e2) -> 
@@ -138,4 +155,24 @@ let ex8 = Write(Prim("<", CstI 4, FromTo(1, 10)));
 // every(write(4 < (1 to 10)))
 let ex9 = Every(Write(Prim("<", CstI 4, FromTo(1, 10))));
 
+(* Exercise 11.8, micro-Icon *)
+
+// dotnet fsi Icon.fs
+// open Icon;;
+// run ex1;;
+
+// (i)
 let ex11_8_ia = Every(Write(Prim("+", CstI 1, (Prim("*", CstI 2, FromTo(1, 4))))));
+
+let ex11_8_ib = Every(Write(Prim("+", Prim("*", CstI 10, FromTo(2, 4)), FromTo(1, 2))));
+
+// (ii)
+let ex11_8_iia = Write(Prim("<", CstI 50, (Prim("*", CstI 7, FromTo(1, 10)))));
+
+// (iii)
+let ex11_8_iiia = Every(Write(Prim1("sqr", FromTo(3, 6))));;
+
+let ex11_8_iiib = Every(Write(Prim1("even", FromTo(1, 7))));;
+
+// (iiii)
+let ex11_8_iiiia = Every(Write(Prim1("multiples", CstI 3)));;
--- BPRD-10/Cont/ex11_8.fs
+++
@@ -1,4 +0,0 @@
-(* Exercise 11.8, micro-Icon *)
-
-// (i)
-let exI = Every(Write(Prim("+", CstI 3, FromTo(1, 4))));
